3d5111386248f5a7d9d795aa1588b6a8039414b9
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-22 12:22:55 +0200

    get img by saving to clipboard

---
+++ game_control/window_cap.py
@@ -0,0 +1,64 @@
+from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
+import cv2 as cv
+import numpy
+import time
+from PIL import Image, ImageGrab 
+import os
+import pyautogui
+import matplotlib.pyplot as plt
+
+windowName = "Ryujinx"
+screen_width, screen_height = 1728.0, 44.0
+
+def findWindowId(windowName):
+    window_list = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID)
+
+    # # Iterate through the list and print window details
+    # for window in window_list:
+    #     if windowName == window['kCGWindowOwnerName']:
+    #         window_id = window.get('kCGWindowNumber')
+    #         window_name = window.get('kCGWindowOwnerName')
+    #         window_size = window.get('kCGWindowBounds', {}).get('Width', 'Unknown'), window.get('kCGWindowBounds', {}).get('Height', 'Unknown')
+    #         print(f"Window ID: {window_id}, Window Name: {window_name}, Window Size: {window_size}")
+
+
+    for window in window_list:
+        if windowName == window['kCGWindowOwnerName']:
+            if window.get('kCGWindowBounds', {}).get('Width', 'Unknown') == screen_width and window.get('kCGWindowBounds', {}).get('Height', 'Unknown') == screen_height:
+                print('found window id %s' % window.get('kCGWindowNumber'))
+                print(window.get('kCGWindowBounds', {}))
+                return window.get('kCGWindowNumber')
+
+    print('unable to find window id')
+    return False
+
+
+def takeScreenshot(windowId):
+    windowId = findWindowId(windowId)
+
+    imageFileName = 'screen.png'
+    # -x mutes sound, -c copies to clipboard, -l specifies windowId
+     # Use screencapture to copy the screen to the clipboard instead of saving to a file
+    os.system('screencapture -c -x -l %s' % windowId)
+
+    # Load the image from the clipboard
+    img = ImageGrab.grabclipboard()
+
+    if img is not None:
+        img_array = numpy.array(img)
+        return img_array
+    else:
+        print("No image on clipboard!")
+        return None
+    
+
+img = takeScreenshot(windowName)
+
+if img is not None:
+    plt.imshow(img)
+    plt.show()
+
+
+# # measure frames per second
+# print('FPS {}'.format(1 / (time() - loopTime)))
+# loopTime = time()
