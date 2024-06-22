49397a20040daf43de578e27f18823666fa35cee
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-22 18:57:34 +0200

    addd mock local server

---
+++ README.md
@@ -0,0 +1,13 @@
+# Usage
+
+1. start local server
+
+```
+uvicorn main:app --reload
+```
+
+2. start ngrok port forwarding
+
+```
+ngrok http http://127.0.0.1:8000
+```
---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
