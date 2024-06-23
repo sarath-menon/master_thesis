12d8e7382607b5eb585deeb0ff493dee0ca0891f
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-23 21:50:13 +0200

    added game contrl class

--- gradio_app.py
+++ gradio_app.py
@@ -3,12 +3,15 @@ import gradio as gr
 from PIL import Image
 import requests
 from io import BytesIO
+from game_control import GameController
 
 URL = "http://localhost:8086/screenshot"
 
+gc = GameController()
+
 def sepia():
-    response = requests.get(URL)
-    img = Image.open(BytesIO(response.content))
+    game_screenshot = gc.get_screenshot()
+    img = Image.open(BytesIO(game_screenshot))
     return np.array(img), "Hello"
 
 demo = gr.Interface(fn=sepia, inputs=[], outputs=[
