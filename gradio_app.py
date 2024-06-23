5aaf4e97a451ffa901581b3957553711705d65b2
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-23 22:04:15 +0200

    calling GPT4-o on img works

--- gradio_app.py
+++ gradio_app.py
@@ -4,15 +4,24 @@ from PIL import Image
 import requests
 from io import BytesIO
 from game_control import GameController
+from models import GPT4OModel
+import base64
 
 URL = "http://localhost:8086/screenshot"
 
 gc = GameController()
+model = GPT4OModel()
 
-def sepia():
+async def sepia():
     game_screenshot = gc.get_screenshot()
+    print("Got game screenshot")
     img = Image.open(BytesIO(game_screenshot))
-    return np.array(img), "Hello"
+
+    base64_image = base64.b64encode(game_screenshot).decode('utf-8')
+    response = await model.generate_response(base64_image)
+    content = response.choices[0].message.content
+    print("Got response")
+    return np.array(img), content
 
 demo = gr.Interface(fn=sepia, inputs=[], outputs=[
     gr.Image(),
