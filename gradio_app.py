425843a1e05b26588f2d3815421b37da046e1de1
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-23 22:12:13 +0200

    showing img sequence works

--- gradio_app.py
+++ gradio_app.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from game_control import GameController
 from models import GPT4OModel
 import base64
+import asyncio
 
 URL = "http://localhost:8086/screenshot"
 
@@ -13,15 +14,21 @@ gc = GameController()
 model = GPT4OModel()
 
 async def sepia():
-    game_screenshot = gc.get_screenshot()
-    print("Got game screenshot")
-    img = Image.open(BytesIO(game_screenshot))
+    while True:
+        game_screenshot = gc.get_screenshot()
+        img = Image.open(BytesIO(game_screenshot))
 
-    base64_image = base64.b64encode(game_screenshot).decode('utf-8')
-    response = await model.generate_response(base64_image)
-    content = response.choices[0].message.content
-    print("Got response")
-    return np.array(img), content
+        base64_image = base64.b64encode(game_screenshot).decode('utf-8')
+
+        # response = await model.generate_response(base64_image)
+        # content = response.choices[0].message.content
+        # print("Got response")
+
+        content = "Hello"
+
+
+        await asyncio.sleep(0.1)  # Sleep for 0.1 seconds (10 times a second)
+        yield np.array(img), content
 
 demo = gr.Interface(fn=sepia, inputs=[], outputs=[
     gr.Image(),
