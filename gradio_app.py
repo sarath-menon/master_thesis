351d08e06ec3305dd0542ab231d27c2412549003
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-24 07:51:29 +0200

    added stream/singe img functions

--- gradio_app.py
+++ gradio_app.py
@@ -13,24 +13,34 @@ URL = "http://localhost:8086/screenshot"
 gc = GameController()
 model = GPT4OModel()
 
-async def sepia():
+async def single_game_screenshot(text):
+    game_screenshot = gc.get_screenshot()
+    print("Got game screenshot")
+    img = Image.open(BytesIO(game_screenshot))
+
+    base64_image = base64.b64encode(game_screenshot).decode('utf-8')
+    response = await model.generate_response(base64_image)
+    content = response.choices[0].message.content
+    print("Got response")
+    return np.array(img), content
+
+async def stream_game_screenshot(text):
     while True:
         game_screenshot = gc.get_screenshot()
         img = Image.open(BytesIO(game_screenshot))
 
         base64_image = base64.b64encode(game_screenshot).decode('utf-8')
 
-        # response = await model.generate_response(base64_image)
-        # content = response.choices[0].message.content
-        # print("Got response")
+        response = await model.generate_response(base64_image)
+        content = response.choices[0].message.content
+        print("Got response")
 
-        content = "Hello"
+        # content = "Hello"
 
-
-        await asyncio.sleep(0.1)  # Sleep for 0.1 seconds (10 times a second)
+        await asyncio.sleep(0.01)  # Sleep for 0.1 seconds (10 times a second)
         yield np.array(img), content
 
-demo = gr.Interface(fn=sepia, inputs=[], outputs=[
+demo = gr.Interface(fn=single_game_screenshot, inputs=["text"], outputs=[
     gr.Image(),
     gr.Code()]
     )
