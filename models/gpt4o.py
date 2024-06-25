3c72fbe7e578f709c5c0b0cd00fce79f4016ca49
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-25 13:31:42 +0200

    removed file watcher

--- models/gpt4o.py
+++ models/gpt4o.py
@@ -3,9 +3,6 @@ from openai import AsyncOpenAI
 import asyncio
 from . import utils
 import json
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-import atexit
 
 class GPT4OModel:    
     def __init__(self, prompt_path):
@@ -15,34 +12,6 @@ class GPT4OModel:
             api_key=os.environ.get("OPENAI_API_KEY"),
         )
         self.prompts_dict = utils.markdown_to_dict(self.PROMPT_PATH)
-        self.observer = None
-
-        self._start_watcher()
-
-    def _start_watcher(self):
-        if self.observer is None:
-            event_handler = self._FileChangeHandler(self)
-            self.observer = Observer()
-            self.observer.schedule(event_handler, path=os.path.dirname(self.PROMPT_PATH), recursive=False)
-            self.observer.start()
-            atexit.register(self._stop_watcher)
-
-    def _stop_watcher(self):
-        if self.observer is not None:
-            self.observer.stop()
-            self.observer.join()
-            self.observer = None
-
-    class _FileChangeHandler(FileSystemEventHandler):
-        def __init__(self, model_instance):
-            self.model_instance = model_instance
-
-        def on_modified(self, event):
-            print("Prompt file modified")
-            if event.src_path == self.model_instance.PROMPT_PATH:
-                self.model_instance.prompts_dict = utils.markdown_to_dict(self.model_instance.PROMPT_PATH)
-            
-            print(self.model_instance.prompts_dict)
 
     async def generate_response(self, base64_image):
         stream = await self.client.chat.completions.create(
