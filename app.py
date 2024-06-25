5395279e1d1578e3983d680911c61d5d69c0d56c
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-25 15:42:33 +0200

    added chatbot

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ import asyncio
 import datetime
 import os
 import json
+import time
 
 URL = "http://localhost:8086/screenshot"
 
 gc = GameController()
-model = GPT4OModel()
+model = GPT4OModel('prompts/mario-odessey.md')
 bbox_model = Florence2Model()
 
 def post_screenshot_callback(image, content):
@@ -36,8 +37,6 @@ async def get_bboxes(image_array, text_input):
     return bbox_image
 
 async def single_game_screenshot(dummy_call=True):
-
-
     game_screenshot = gc.get_screenshot()
     print("Got game screenshot")
     img = Image.open(BytesIO(game_screenshot))
@@ -107,22 +106,74 @@ def update_direction_options(action):
         return gr.update(choices=["forward", "backward", "left", "right"])
     return gr.update(choices=[])
 
+# async def slow_echo(message, history):
+#     # pause game
+#     gc.pause_game()
+
+#     # # get model output and game screenshot
+#     # (img, content) = await single_game_screenshot()
+
+#     chunk = await model.generate_waste_async(message)
+#     print(chunk)
+
+#     # async for chunk in stream:
+#     #     yield chunk.choices[0].delta.content or ""
+
+async def slow_echo(message, history, dummy_call=True):
+
+    # pause game
+    gc.pause_game()
+
+    game_screenshot = gc.get_screenshot()
+    print("Got game screenshot")
+    img = Image.open(BytesIO(game_screenshot))
+
+    # call model
+    base64_image = base64.b64encode(game_screenshot).decode('utf-8')
+    stream = await model.generate_response_async(base64_image)
+    # content = response.choices[0].message.content
+    # content = json.loads(content)
+
+    # stream = await model.generate_waste_async(message)
+
+    # print response
+    response = ""
+    async for chunk in stream:
+        content = chunk.choices[0].delta.content or ""
+        response += content
+        yield response
+
+    content = json.loads(response)
+    print(content)
+    
+    # do action
+    await do_action(content["action"], content["direction"])
+        
 with gr.Blocks() as demo:
     gr.Markdown("# Game Screenshot and Response")
-    with gr.Row():
-        with gr.Tab("VLM Input"):
-            vlm_input = gr.Image(show_label=False)
-        with gr.Tab("Bounding Boxes"):
-            bbox_output = gr.Image(show_label=False)
+    # with gr.Row():
+    #     with gr.Tab("VLM Input"):
+    #         vlm_input = gr.Image(show_label=False)
+    #     with gr.Tab("Bounding Boxes"):
+    #         bbox_output = gr.Image(show_label=False)
+
+    with gr.Column(scale=2):
+        chatbot = gr.Chatbot(render=False)
+
+        gr.ChatInterface(
+        fn=slow_echo,
+        examples=[{"text": "hello"}, {"text": "hola"}, {"text": "merhaba"}],
+            chatbot=chatbot
+        )
 
     with gr.Row():
-        with gr.Column():
-            gr.Markdown("## Model output")
-            # text_input = gr.Textbox(container=False, lines=6)
-            code_output = gr.Textbox(container=False, lines=6)
-            submit_button = gr.Button("Submit")
-            debug_button = gr.Button("Print Model Prompts")
-            debug_button.click(fn=lambda: print(model.prompts_dict))
+    #     with gr.Column():
+    #         gr.Markdown("## Model output")
+    #         # text_input = gr.Textbox(container=False, lines=6)
+    #         # code_output = gr.Textbox(container=False, lines=6)
+    #         submit_button = gr.Button("Submit")
+    #         # debug_button = gr.Button("Print Model Prompts")
+    #         # debug_button.click(fn=lambda: print(model.prompts_dict))
 
         with gr.Column():
             gr.Markdown("## Select action manually")
@@ -142,9 +193,9 @@ with gr.Blocks() as demo:
                 resume_button.click(fn=gc.resume_game)
 
 
-    submit_button.click(fn=single_game_screenshot, inputs=[], outputs=[vlm_input, code_output]).then(
-        fn=get_bboxes, inputs=[vlm_input, code_output], outputs=[bbox_output]
-    )
+            # submit_button.click(fn=single_game_screenshot, inputs=[], outputs=[vlm_input, code_output]).then(
+            #     fn=get_bboxes, inputs=[vlm_input, code_output], outputs=[bbox_output]
+            # )
 
     # .then(
     #     fn=save_image_and_response, inputs=[vlm_input, code_output]
