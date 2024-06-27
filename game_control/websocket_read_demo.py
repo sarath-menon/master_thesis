e1e6bb0dd6863e3bcb617f9ca3cbfe7fa5b57e6c
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-27 17:23:07 +0200

    added FPS printing

--- game_control/websocket_read_demo.py
+++ game_control/websocket_read_demo.py
@@ -6,7 +6,12 @@ from PIL import Image
 import cv2
 import numpy as np
 
+frame_count = 0
+start_time = time.time()
+
 def on_message(ws, message):
+    global frame_count, start_time
+    
     # Assuming the server sends binary JPEG images
     image_stream = io.BytesIO(message)
     image = Image.open(image_stream)
@@ -20,6 +25,15 @@ def on_message(ws, message):
     cv2.imshow('frame', img_np)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
+
+
+    frame_count += 1
+    elapsed_time = time.time() - start_time
+    if elapsed_time > 1:  # Update FPS every second
+        fps = frame_count / elapsed_time
+        print(f"FPS: {fps:.2f}")
+        frame_count = 0
+        start_time = time.time()
 
 def on_error(ws, error):
     print(f"Error: {error}")
