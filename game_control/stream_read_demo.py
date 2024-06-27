50bcf30476c8fbf2631bc4f0d894732cdda28514
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-27 13:57:32 +0200

    added timestamp in http video stream

--- game_control/stream_read_demo.py
+++ game_control/stream_read_demo.py
@@ -13,13 +13,16 @@ def stream_frames(url):
             print("Failed to connect to the server.")
             return
 
+        print(response.headers['X-Timestamp'])
+
         boundary = response.headers['content-type'].split('=')[1]
         buffer = b''
         
         for chunk in response.iter_content(chunk_size=1024):
             buffer += chunk
             while True:
-                boundary_index = buffer.find(b'--' + boundary.encode())
+                boundary_bytes = b'--' + boundary.encode()
+                boundary_index = buffer.find(boundary_bytes)
                 if boundary_index == -1:
                     break
                 
@@ -27,11 +30,13 @@ def stream_frames(url):
                 if header_end_index == -1:
                     break
                 
-                next_boundary_index = buffer.find(b'--' + boundary.encode(), header_end_index + 4)
+                next_boundary_index = buffer.find(boundary_bytes, header_end_index + 4)
                 if next_boundary_index == -1:
                     break
                 
                 frame_data = buffer[header_end_index + 4:next_boundary_index]
+                buffer = buffer[next_boundary_index:]  # Move the buffer pointer to the next part
+                
                 img_np = cv2.imdecode(np.frombuffer(frame_data, np.uint8), cv2.IMREAD_COLOR)
                 cv2.imshow('frame', img_np)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
