18f700322e760c47afa3f82d68e8377892035a9e
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-28 18:08:40 +0200

    added error handling and socket closing

--- game_control/websocket_client_without_app.py
+++ game_control/websocket_client_without_app.py
@@ -12,15 +12,24 @@ image_data = b'...'  # This should be your _imageByte data
 width = 1920      # Replace with actual image width
 height = 1080        # Replace with actual image height
 
+
+
 def main(ws, n_frames):
 
     global frame_count, start_time
 
     message = {"action": "request_frames", "count": n_frames}
-    ws.send(json.dumps(message))
+    try:
+        ws.send(json.dumps(message))
+    except websocket.WebSocketException as e:
+        print(f"Failed to send message: {e}")
+        return
 
     for i in range(n_frames):
-        message = ws.recv()
+        try:
+            message = ws.recv()
+        except websocket.WebSocketTimeoutException:
+            print("Timeout occurred while waiting for a message")
 
         # Create an image from the byte data
         image = Image.frombytes(mode='RGBA', size=(width, height), data=message)
@@ -41,8 +50,11 @@ if __name__ == "__main__":
     websocket.enableTrace(False)
     ws = websocket.WebSocket()
     ws.connect("ws://localhost:8086/stream_websocket")
+    # ws.settimeout(2)# Set the timeout to 2 seconds 
 
     n_frames = 100
-
     main(ws, n_frames)
 
+    # Close the WebSocket connection gracefully
+    if ws and ws.connected:
+        ws.close()
