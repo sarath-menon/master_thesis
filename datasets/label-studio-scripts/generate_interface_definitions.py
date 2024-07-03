57ea63a8642014cdf5dba9a7bae72c1efb65557f
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-07-03 18:06:02 +0200

    added color code for objects

--- datasets/label-studio-scripts/generate_interface_definitions.py
+++ datasets/label-studio-scripts/generate_interface_definitions.py
@@ -1,7 +1,22 @@
 import xml.etree.ElementTree as ET
 
 # Define the label values
-label_values = ["door", "window", "sword", "treasure", "bush", "tree", "pillow", "book", "box", "sphere", "bird", "barrel", "painting", "stick", "moon", "lamp", "instrument", "table", "chair", "skull", "telescope", "globe", "mirror", "lamp", "vase", "hat", "moon", "pillar", "crate", "coin"]
+label_values = ["door", "window", "sword", "treasure", "bush", "tree", "pillow", "book", "box", "sphere", "bird", "barrel", "painting", "stick", "moon", "lamp", "instrument", "table", "chair", "skull", "telescope", "globe", "mirror", "vase", "hat", "pillar", "crate", "coin", "watertank", "ring", "cannon", "fire_hydrant", "cap", "car", "plant", "flag", "watertank", "can"]
+
+import random
+
+# Assign random colour code for each label
+label_colors = {label: f"#{hash(label) & 0xFFFFFF:06x}" for label in label_values}
+
+# Remove duplicates in label_values and print the duplicate values
+unique_label_values = list(set(label_values))
+duplicates = [label for label in label_values if label_values.count(label) > 1]
+label_values = unique_label_values
+if duplicates:
+    print("Duplicate values: ", duplicates)
+
+# sort the label values in alphabetical order
+label_values.sort()
 
 # Create the root element
 root = ET.Element("View")
@@ -28,7 +43,7 @@ polygon_labels.set("opacity", "0.9")
 for label_value in label_values:
     label = ET.SubElement(polygon_labels, "Label")
     label.set("value", label_value)
-    label.set("background", "#FFA39E")  # You can change the background color here
+    label.set("background", label_colors[label_value]) 
 
 # # Add the RectangleLabels element
 # rectangle_labels = ET.SubElement(root, "RectangleLabels")
