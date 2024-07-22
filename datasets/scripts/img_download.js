e684acde29fe04d01b2e9d80dd0412028fdb72d0
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-07-22 21:39:19 +0200

    works, but download same image multiple times

--- datasets/scripts/img_download.js
+++ datasets/scripts/img_download.js
@@ -1,20 +1,40 @@
+
+// Function to create a blocking delay
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+for (let i = 0; i < 4; i++) {
+    
 // Select all images within #galleryOverlay that have a .jpg source
-let images = document.querySelectorAll('#galleryOverlay img[src$=".jpg"]');
+let images = document.querySelectorAll('img[src$=".jpg"]');
 
-let img = images[2];
-if (img) {
-    var link = document.createElement('a');
-    link.href = img.src;
+for (let img of images) {
+    if (img.naturalWidth === 1920) {
+        console.log(img.src);
 
-    // Extract the filename from the image source
-    var filename = img.src.split('/').pop(); // Get the last segment after the last slash
-    link.download = filename; // Use the actual image filename
+        // Print the resolution of the image
+        console.log(`Resolution: ${img.naturalWidth} x ${img.naturalHeight}`);
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+        var link = document.createElement('a');
+        link.href = img.src;
+
+        // Extract the filename from the image source
+        var filename = img.src.split('/').pop(); // Get the last segment after the last slash
+        link.download = filename; // Use the actual image filename
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 }
 
 // If you need to simulate right arrow key press for navigation, consider doing it outside the loop
 let e = new KeyboardEvent("keydown", {bubbles: true, cancelable: true, key: "ArrowRight", keyCode: 39});
 document.dispatchEvent(e);
+
+// Add a delay of 1 second
+setTimeout(function() {}, 1000);
+
+await delay(1000);
+}
