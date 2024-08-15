1b525095296de906778557903e1be0f93cb7267c
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-15 15:22:37 +0200

    set operation_id to generate client functions

--- bases/clicking/api/routes/localization.py
+++ bases/clicking/api/routes/localization.py
@@ -4,25 +4,24 @@ from clicking.localization.core import PredictionReq, PredictionResp, Localizati
 localization_router = APIRouter()
 localization_model = LocalizationModel()
 
-@localization_router.get("/localization", response_model=PredictionResp)
-async def localization(req: PredictionReq):
+@localization_router.get("/prediction", response_model=PredictionResp, operation_id="get_localization_prediction")
+async def get_prediction(req: PredictionReq):
     return await localization_model.get_localization(req)
 
-
-@localization_router.get("/models", response_model=ModelsResp)
-async def get_localization_model():
+@localization_router.get("/models", response_model=ModelsResp, operation_id="get_available_localization_models")
+async def get_models():
     models = localization_model.get_available_models()
     return models
 
-@localization_router.get("/model", response_model=ModelInfo)
-async def get_localization_model():
+@localization_router.get("/model", response_model=ModelInfo, operation_id="get_localization_model")
+async def get_model():
     model = localization_model.get_model()
     return {"model_name": model.name,
      "model_variant": model.variant,
      "model_tasks": model.tasks()}
 
-@localization_router.post("/model")
-async def set_localization_model(req: dict):
+@localization_router.post("/model", operation_id="set_localization_model")
+async def set_model(req: dict):
     model_name = req.get('model_name')
     model_variant = req.get('model_variant')
     try:
