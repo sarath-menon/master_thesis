82fe64b2a343b69f06ffc92643edb1a14a03b186
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-21 09:42:30 +0200

    cleanup

--- development/scripts/instruction_to_label.py
+++ development/scripts/instruction_to_label.py
@@ -2,6 +2,7 @@
 from components.clicking.prompt_manager.core import PromptManager
 from PIL import Image
 from matplotlib import pyplot as plt
+from enum import Enum, auto
 
 # %%
 image = Image.open("./datasets/resized_media/gameplay_images/mario_odessey/8.jpg")
@@ -14,7 +15,13 @@ from litellm import completion
 import io
 import base64
 
+class PromptMode(Enum):
+    LABEL = auto()
+    EXPANDED_DESCRIPTION = auto()
+
 class PromptRefiner:
+
+    
     def __init__(self, prompt_path: str, model: str = "gpt-4o" ):
         self.model = model
         self.PROMPT_PATH = prompt_path
@@ -43,22 +50,25 @@ class PromptRefiner:
             image.save(buffer, format="PNG")
             return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
-    def get_label(self, screenshot: str, action: str):
-        template_values = {
-        "action": action
-        }
+    def process_prompt(self, screenshot: str, input_text: str, mode: PromptMode, word_limit: int = 10):
+        if mode == PromptMode.LABEL:
+            return self.get_label(screenshot, input_text)
+        elif mode == PromptMode.EXPANDED_DESCRIPTION:
+            return self.get_expanded_description(screenshot, input_text, word_limit)
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
 
+    def get_label(self, screenshot: str, action: str):
+        template_values = {"action": action}
         base64_image = self._pil_to_base64(screenshot)
         prompt = self.prompt_manager.get_prompt(type='user', prompt_key='default', template_values=template_values)
-        print(prompt)
-        # return self._get_image_response(base64_image, prompt)
+        return self._get_image_response(base64_image, prompt)
 
-    def get_expanded_description(self, screenshot: str, input_description: str):
+    def get_expanded_description(self, screenshot: str, input_description: str, word_limit: int = 10):
         template_values = {
-        "input_description": input_description,
-        "word_limit": "10"
+            "input_description": input_description,
+            "word_limit": str(word_limit)
         }
-
         base64_image = self._pil_to_base64(screenshot)
         prompt = self.prompt_manager.get_prompt(type='user', prompt_key='prompt_expansion', template_values=template_values)
         return self._get_image_response(base64_image, prompt)
@@ -70,7 +80,7 @@ class PromptRefiner:
 labeller =  PromptRefiner(prompt_path="./prompts/instruction_refinement.md")
 
 # response = labeller.get_label(image, "Pick up the flag")
-response = labeller.get_expanded_description(image, "yellow car")
+response = labeller.process_prompt(image, "yellow car", PromptMode.EXPANDED_DESCRIPTION)
 print(response)
 # %%
 import re
