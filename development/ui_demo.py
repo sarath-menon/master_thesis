5c63fb000b999c6f0e95a2e66c308140be756811
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-22 09:44:00 +0200

    better folder organization

---
+++ development/ui_demo.py
@@ -0,0 +1,196 @@
+#%%
+from transformers import AutoProcessor, AutoModelForCausalLM
+from PIL import Image, ImageDraw, ImageFont 
+import requests
+import copy
+import os
+import random
+import numpy as np
+
+from transformers.dynamic_module_utils import get_imports
+from unittest.mock import patch
+import base64
+import io
+
+%matplotlib inline
+#%%
+model_id = 'microsoft/Florence-2-base-ft'
+images_path = "./datasets/raw_media/ui_dataset"
+
+def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
+    """Work around for https://huggingface.co/microsoft/phi-1_5/discussions/72."""
+    if not str(filename).endswith("/modeling_florence2.py"):
+        return get_imports(filename)
+    imports = get_imports(filename)
+    imports.remove("flash_attn")
+    return imports
+
+with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports):
+        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+        processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+colormap = ['blue','orange','green','purple','brown','pink','gray','olive','cyan','red',
+            'lime','indigo','violet','aqua','magenta','coral','gold','tan','skyblue']
+
+def run_example(task_prompt, text_input=None):
+    if text_input is None:
+        prompt = task_prompt
+    else:
+        prompt = task_prompt + text_input
+    inputs = processor(text=prompt, images=image, return_tensors="pt")
+    generated_ids = model.generate(
+      input_ids=inputs["input_ids"],
+      pixel_values=inputs["pixel_values"],
+      max_new_tokens=1024,
+      early_stopping=False,
+      do_sample=False,
+      num_beams=3,
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+    parsed_answer = processor.post_process_generation(
+        generated_text,
+        task=task_prompt,
+        image_size=(image.width, image.height)
+    )
+
+    return parsed_answer
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+def plot_bbox(image, data):
+   # Create a figure and axes
+    fig, ax = plt.subplots()
+
+    # Display the image
+    ax.imshow(image)
+
+    # Plot each bounding box
+    for bbox, label in zip(data['bboxes'], data['labels']):
+        # Unpack the bounding box coordinates
+        x1, y1, x2, y2 = bbox
+        # Create a Rectangle patch
+        rect = patches.Rectangle((x1, y1), x2-x1, y2-y1, linewidth=1, edgecolor='r', facecolor='none')
+        # Add the rectangle to the Axes
+        ax.add_patch(rect)
+        # Annotate the label
+        plt.text(x1, y1, label, color='white', fontsize=8, bbox=dict(facecolor='red', alpha=0.5))
+
+    # Remove the axis ticks and labels
+    ax.axis('off')
+
+
+    # Show the plot
+    plt.show()
+
+def convert_to_od_format(data):  
+    """  
+    Converts a dictionary with 'bboxes' and 'bboxes_labels' into a dictionary with separate 'bboxes' and 'labels' keys.  
+  
+    Parameters:  
+    - data: The input dictionary with 'bboxes', 'bboxes_labels', 'polygons', and 'polygons_labels' keys.  
+  
+    Returns:  
+    - A dictionary with 'bboxes' and 'labels' keys formatted for object detection results.  
+    """  
+    # Extract bounding boxes and labels  
+    bboxes = data.get('bboxes', [])  
+    labels = data.get('bboxes_labels', [])  
+      
+    # Construct the output format  
+    od_results = {  
+        'bboxes': bboxes,  
+        'labels': labels  
+    }  
+      
+    return od_results  
+
+def draw_ocr_bboxes(image, prediction):
+    scale = 1
+    draw = ImageDraw.Draw(image)
+    bboxes, labels = prediction['quad_boxes'], prediction['labels']
+    for box, label in zip(bboxes, labels):
+        color = random.choice(colormap)
+        new_box = (np.array(box) * scale).tolist()
+        draw.polygon(new_box, width=3, outline=color)
+        draw.text((new_box[0]+8, new_box[1]+2),
+                    "{}".format(label),
+                    align="right",
+        
+                    fill=color)
+    display(image)
+
+
+def image_to_base64(img):
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+#%%
+
+from openai import OpenAI
+
+MODEL = "gpt-4o"
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+system_prompt = "You are a helpful assistant and an expert in playing videogames."
+
+def generate_user_prompt(action: str):
+    return f"""The image shows a video game's title screen. The task is to {action} by manipulating the UI elements in one of the following ways: [button clicking, slider dragging, filling text input]. Provide a 20-word reasoning. Describe the element's name and give a detailed graphical description. Format the response as JSON with keys: action, element_name, element_description, reasoning.
+    """
+
+def get_response(image, action: str):
+    base64_image = image_to_base64(image)
+    user_prompt = generate_user_prompt(action)
+
+    stream = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": [
+                {"type": "text", "text": user_prompt},
+                {"type": "image_url", "image_url": {
+                    "url": f"data:image/png;base64,{base64_image}"}
+                }
+            ]} 
+        ],
+        stream=True,
+        response_format={"type": "json_object"},
+        temperature=0.0,
+    )
+
+    response = ""
+    for chunk in stream:
+        content = chunk.choices[0].delta.content
+        if content is not None:
+            response += content
+            print(content, end="")
+    return response
+
+# %%selecting the image
+image = Image.open(images_path + "/stats_resources/inventory/5.jpg").convert("RGB")
+plt.imshow(image)
+plt.grid(False)
+plt.axis('off')
+plt.show()
+
+# %%
+## Pre-processing
+
+# action = "minimize the sound effects volume"
+action = "start a new game"
+response = get_response(image, action)
+# %% OCR
+
+task_prompt = '<OCR_WITH_REGION>'
+results = run_example(task_prompt)
+print(results['<OCR_WITH_REGION>']['labels'])
+draw_ocr_bboxes(image, results['<OCR_WITH_REGION>'])
+# %%
+
+task_prompt = '<CAPTION_TO_PHRASE_GROUNDING>'
+text_input="umbrella."
+results = run_example(task_prompt, text_input=text_input)
+print(results)
+plot_bbox(image, results['<CAPTION_TO_PHRASE_GROUNDING>'])
