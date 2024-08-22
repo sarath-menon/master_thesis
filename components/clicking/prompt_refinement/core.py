2584a9009428d8c50738da0b6baabbb9202d8904
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-22 17:20:30 +0200

    added sample code in comments

--- components/clicking/prompt_refinement/core.py
+++ components/clicking/prompt_refinement/core.py
@@ -1,3 +1,4 @@
+#%%
 from litellm import completion
 import os
 import dotenv
@@ -71,3 +72,19 @@ class PromptRefiner:
     def show_messages(self):
         for message in self.messages:
             print(message)
+# # %%
+
+# ## Sample code
+
+# from PIL import Image
+# from matplotlib import pyplot as plt
+
+# image = Image.open("./datasets/resized_media/gameplay_images/mario_odessey/8.jpg")
+# plt.grid(False)
+# plt.axis('off')
+# plt.imshow(image)
+
+# labeller =  PromptRefiner(prompt_path="./prompts/prompt_refinement.md")
+# response = labeller.process_prompt(image, "yellow car", PromptMode.EXPANDED_DESCRIPTION)
+# print(response)
+
