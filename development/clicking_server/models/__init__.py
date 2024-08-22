98900953bce4056ec4cd1d136f5f3ffe82de955d
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-22 18:08:11 +0200

    cleanup

--- development/clicking_server/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.florence2 import Florence2Model
-from models.grounding_dino import GroundingDinoModel
-from models.sam2 import SAM2Model
--- development/model_utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from model_utils.visualization import *
