a3a6ce6777fbd49ee61c16dbbea633dd5dfbed53
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-23 13:25:53 +0200

    added get_mask_centroid function

--- components/clicking/vision_model/utils.py
+++ components/clicking/vision_model/utils.py
@@ -1,9 +1,24 @@
 import numpy as np
 from typing import Dict, Any
 from pycocotools import mask as mask_utils
+import numpy as np
+from scipy.ndimage import center_of_mass
+from scipy.ndimage import distance_transform_edt
+
 
 def coco_encode_rle(mask: np.ndarray) -> Dict[str, Any]:
     binary_mask = mask.astype(bool)
     rle = mask_utils.encode(np.asfortranarray(binary_mask))
     rle['counts'] = rle['counts'].decode('utf-8')
     return rle
+
+
+def get_mask_centroid(mask):
+    # Compute the distance transform
+    distances = distance_transform_edt(mask)
+    
+    # Find the maximum distance, and thus the center of the largest inscribed circle
+    max_dist_idx = np.unravel_index(np.argmax(distances), distances.shape)
+    circle_center = (max_dist_idx[1], max_dist_idx[0])  # (x, y) format
+
+    return circle_center
