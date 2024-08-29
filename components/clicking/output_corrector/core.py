4b2fb23bc143590bc6e6cd447190ee1441f90ce4
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-29 19:15:39 +0200

    fixed import path

--- components/clicking/output_corrector/core.py
+++ components/clicking/output_corrector/core.py
@@ -53,7 +53,7 @@ class OutputCorrector(ImageProcessorBase):
 if __name__ == "__main__":
     from PIL import Image
     import matplotlib.pyplot as plt
-    from clicking.vision_model.core import overlay_bounding_box
+    from clicking.vision_model.visualization import overlay_bounding_box
     from clicking.vision_model.bbox import BoundingBox, BBoxMode
     import asyncio
     import nest_asyncio
