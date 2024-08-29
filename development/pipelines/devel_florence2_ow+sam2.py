b4c97e4470c5270d1b4886b7989214fd20013980
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-08-29 11:57:32 +0200

    added accidentally deleted code

--- development/pipelines/devel_florence2_ow+sam2.py
+++ development/pipelines/devel_florence2_ow+sam2.py
@@ -39,6 +39,74 @@ client = Client(base_url="http://localhost:8082")
 prompt_refiner = PromptRefiner(prompt_path="./prompts/prompt_refinement.md")
 
 coco_dataset = CocoDataset('./datasets/label_studio_gen/coco_dataset/images', './datasets/label_studio_gen/coco_dataset/result.json')
+#%%
+
+from clicking_client.types import File
+from io import BytesIO
+
+def image_to_http_file(image):
+    # Convert PIL Image to bytes and create a File object
+    image_byte_arr = BytesIO()
+    image.save(image_byte_arr, format='JPEG')
+    image_file = File(file_name="image.jpg", payload=image_byte_arr.getvalue(), mime_type="image/jpeg")
+    return image_file
+
+class LocalizationProcessor:
+    def __init__(self, client: Client):
+        self.client = client
+
+    def get_localization_results(self, processed_result: ProcessedResult) -> LocalizationResults:
+        set_model.sync(client=self.client, body=SetModelReq(name="florence2", variant="florence-2-base", task=TaskType.LOCALIZATION_WITH_TEXT_OPEN_VOCAB))
+        
+        localization_results = {}
+        for sample in processed_result.samples:
+            image_file = image_to_http_file(sample.image)
+            predictions = {}
+
+            for obj in sample.description["objects"]:
+                request = BodyGetPrediction(
+                    image=image_file,
+                    task=TaskType.LOCALIZATION_WITH_TEXT_OPEN_VOCAB,
+                    input_text=obj["description"]
+                )
+                response = get_prediction.sync(client=self.client, body=request)
+                predictions[obj["name"]] = LocalizationPrediction(bboxes=response.prediction.bboxes)
+            
+            image_filename = sample.image.filename if hasattr(sample.image, 'filename') else f"image_{id(sample.image)}"
+            localization_results[image_filename] = predictions
+        
+        return LocalizationResults(
+            processed_samples=processed_result.samples,
+            localization_results=localization_results
+        )
+
+class SegmentationProcessor:
+    def __init__(self, client: Client):
+        self.client = client
+
+    def get_segmentation_results(self, data: LocalizationResults) -> SegmentationResults:
+        set_model.sync(client=self.client, body=SetModelReq(name="sam2", variant="sam2_hiera_tiny", task=TaskType.SEGMENTATION_WITH_BBOX))
+        
+        segmentation_results = {}
+        for sample in data.processed_samples:
+            image_file = image_to_http_file(sample.image)
+            image_filename = sample.image.filename if hasattr(sample.image, 'filename') else f"image_{id(sample.image)}"
+            seg_predictions = {}
+            for obj_name, loc_result in data.localization_results[image_filename].items():
+                request = BodyGetPrediction(
+                    image=image_file,
+                    task=TaskType.SEGMENTATION_WITH_BBOX,
+                    input_boxes=loc_result.bboxes
+                )
+                response = get_prediction.sync(client=self.client, body=request)
+                seg_predictions[obj_name] = SegmentationPrediction(masks=response.prediction.masks)
+            segmentation_results[image_filename] = seg_predictions
+        
+        return SegmentationResults(
+            processed_samples=data.processed_samples,
+            localization_results=data.localization_results,
+            segmentation_results=segmentation_results
+        )
 
 #%%
 
