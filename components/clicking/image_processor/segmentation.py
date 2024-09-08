f8e148ebfc0779ba0c76b007b3344c5c9a7b1d03
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-08 16:24:38 +0200

    segmentation works

--- components/clicking/image_processor/segmentation.py
+++ components/clicking/image_processor/segmentation.py
@@ -1,8 +1,7 @@
-from typing import Dict
+from typing import Dict, List
 from clicking_client import Client
 from clicking_client.models import SetModelReq, BodyGetPrediction
 from clicking_client.api.default import set_model, get_prediction
-from clicking.common.data_structures import *
 from clicking.common.data_structures import PipelineState
 from .utils import image_to_http_file
 from clicking.vision_model.data_structures import TaskType
@@ -14,21 +13,26 @@ class Segmentation:
     def __init__(self, client: Client, config: Dict):
         self.client = client
         self.config = config
+        self.tasks = self.load_tasks()
         self.set_segmentation_model()
+
+    def load_tasks(self) -> List[TaskType]:
+        task_strings = self.config['models']['segmentation']['tasks']
+        return [TaskType[task] for task in task_strings]
 
     def set_segmentation_model(self):
         try:
-            response= set_model.sync(client=self.client, body=SetModelReq(
-                name=self.config['models']['segmentation']['name'],
-                variant=self.config['models']['segmentation']['variant'],
-                task=TaskType[self.config['models']['segmentation']['task']]
-            ))
-            print(response)
+            for task in self.tasks:
+                response = set_model.sync(client=self.client, body=SetModelReq(
+                    name=self.config['models']['segmentation']['name'],
+                    variant=self.config['models']['segmentation']['variant'],
+                    task=task
+                ))
+                print(f"Set model for task {task}: {response}")
         except Exception as e:
             print(f"Error setting segmentation model: {str(e)}")
 
     def get_segmentation_results(self, state: PipelineState) -> PipelineState:
-        
         for clicking_image in state.images:
             image_file = image_to_http_file(clicking_image.image)
             
@@ -39,13 +43,17 @@ class Segmentation:
                         client=self.client,
                         body=request,
                         task=TaskType.SEGMENTATION_WITH_BBOX,
-                        input_boxes=json.dumps(obj.bbox.get(mode=BBoxMode.XYWH))
+                        input_boxes=json.dumps(obj.bbox.get(mode=BBoxMode.XYXY))
                     )
                     
-                    if response.prediction.masks:
-                        obj.mask = SegmentationMask(coco_rle=response.prediction.masks[0], mode=SegmentationMode.COCO_RLE)
-                    else:
+                    # error handling
+                    if len(response.prediction.masks) > 1:
+                        print(f"Multiple masks found for {obj.name}: {len(response.prediction.masks)}. Ignoring.")
+                    elif len(response.prediction.masks) == 0:
                         print(f"No segmentation mask found for {obj.name}")
+
+                    obj.mask = SegmentationMask(coco_rle=response.prediction.masks[0], mode=SegmentationMode.COCO_RLE)
+
                 except Exception as e:
                     print(f"Error processing segmentation for image {clicking_image.id}, object {obj.name}: {str(e)}")
         
