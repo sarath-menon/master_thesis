79550d6b8a60c386941c4c164a34a0cf3b18f3d5
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-08 19:12:55 +0200

    take PipelineState as input instead oc ImageObject type

--- components/clicking/prompt_refinement/core.py
+++ components/clicking/prompt_refinement/core.py
@@ -16,7 +16,7 @@ import json
 from PIL import Image
 import uuid
 from clicking.prompt_refinement.data_structures import *
-from clicking.common.data_structures import ClickingImage, ObjectCategory, ImageObject
+from clicking.common.data_structures import ClickingImage, ObjectCategory, ImageObject, PipelineState
 from pydantic import Field
 from tqdm.asyncio import tqdm
 
@@ -36,7 +36,8 @@ class PromptRefiner(ImageProcessorBase):
         # Load configuration
         self.config = config
         
-    async def process_prompts_async(self, clicking_images: List[ClickingImage], mode: PromptMode = PromptMode.IMAGE_TO_OBJECT_DESCRIPTIONS, **kwargs) -> List[ClickingImage]:
+    async def process_prompts_async(self, state: PipelineState, mode: PromptMode = PromptMode.IMAGE_TO_OBJECT_DESCRIPTIONS, **kwargs) -> PipelineState:
+        clicking_images = state.clicking_images
         images = [ci.image for ci in clicking_images]
         template_values = [self._get_template_values(mode, None, **kwargs) for _ in clicking_images]
 
@@ -109,67 +110,5 @@ class PromptRefiner(ImageProcessorBase):
         for message in self.messages:
             print(message)
 
-    def process_prompts(self, clicking_images: List[ClickingImage], mode: PromptMode = PromptMode.IMAGE_TO_OBJECT_DESCRIPTIONS, **kwargs) -> List[ClickingImage]:
-        return asyncio.run(self.process_prompts_async(clicking_images, mode, **kwargs))
-
-# %% get expanded description from class label
-
-if __name__ == "__main__":
-    from PIL import Image
-    import asyncio
-    import nest_asyncio
-    nest_asyncio.apply()
-
-    image = Image.open("./datasets/resized_media/gameplay_images/mario_odessey/8.jpg")
-
-    # Create an instance of PromptRefiner
-    prompt_refiner = PromptRefiner(prompt_path="./prompts/prompt_refinement.md")
-
-    # Define the batch of screenshots and corresponding input texts
-    screenshots = [image, image]
-    input_texts = [
-        "car",
-        "flag"
-    ]
-
-    # Define the mode and word limit for the prompts
-    mode = PromptMode.OBJECTS_LIST_TO_DESCRIPTIONS
-    word_limit = 5
-
-    # Call process_prompts asynchronously
-    async def process_batch_prompts():
-        results = await prompt_refiner.process_prompts_async(screenshots, mode, input_texts=input_texts, word_limit=word_limit)
-        print(results)
-
-    # Run the asynchronous function
-    asyncio.get_event_loop().run_until_complete(process_batch_prompts())
-
-# %% get class labels from image
-
-if __name__ == "__main__":
-    from PIL import Image
-    import asyncio
-    import nest_asyncio
-    nest_asyncio.apply()
-
-    image_1 = Image.open("./datasets/resized_media/gameplay_images/unpacking/1.jpg")
-    image_2 = Image.open("./datasets/resized_media/gameplay_images/mario_odessey/8.jpg")
-
-    # Create an instance of PromptRefiner
-    prompt_refiner = PromptRefiner(prompt_path="./prompts/prompt_refinement.md")
-
-    # Define the batch of screenshots 
-    images = [image_1, image_2]
-
-    # Define the mode and word limit for the prompts
-    mode = PromptMode.IMAGE_TO_OBJECT_DESCRIPTIONS
-
-      # Call process_prompts asynchronously
-    async def process_batch_prompts():
-        results = await prompt_refiner.process_prompts_async(images, mode)
-        for result in results:
-            print(result)
-
-    # Run the asynchronous function
-    asyncio.get_event_loop().run_until_complete(process_batch_prompts())
-
+    def process_prompts(self, state: PipelineState, mode: PromptMode = PromptMode.IMAGE_TO_OBJECT_DESCRIPTIONS, **kwargs) -> PipelineState:
+        return asyncio.run(self.process_prompts_async(state, mode, **kwargs))
