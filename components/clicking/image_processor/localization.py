097277ef92ece4cc425e1cae00fdf216c56dd95e
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-09 10:03:57 +0200

    fixed description processing

--- components/clicking/image_processor/localization.py
+++ components/clicking/image_processor/localization.py
@@ -9,12 +9,12 @@ from clicking.vision_model.data_structures import TaskType
 from clicking.common.bbox import BoundingBox, BBoxMode
 from enum import Enum
 
-def process_description(name: str):
-    return name.lower() + "."
+def process_description(description: str):
+    return description
 
 class LocalizerInput(Enum):
     OBJ_NAME = ModuleMode("obj_name", lambda obj: obj.name)
-    OBJ_DESCRIPTION = ModuleMode("obj_description", lambda obj: process_description(obj.name))
+    OBJ_DESCRIPTION = ModuleMode("obj_description", lambda obj: process_description(obj.description))
 
 class Localization:
     def __init__(self, client: Client, config: Dict):
