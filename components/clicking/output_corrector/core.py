b3323dbfa160e02dc64df1374f524d6d92933603
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-12 19:46:25 +0200

    removed unnecessary tqdmprogress visualizer

--- components/clicking/output_corrector/core.py
+++ components/clicking/output_corrector/core.py
@@ -65,7 +65,7 @@ class OutputCorrector(ImageProcessorBase):
         processed_images, prompts, messages = [], [], []
         object_names = []
 
-        for obj_dict in async_tqdm(objects.values(), desc="Processing objects"):
+        for obj_dict in objects.values():
             if obj_dict.object.validity.status == ValidityStatus.INVALID:
                 print(f"Warning: Skipping bbox verification for object {obj_dict.object.name} due to invalid bbox.")
                 continue
