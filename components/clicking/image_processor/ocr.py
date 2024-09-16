5572b0a9469d2c35da71883803f16da6a412c050
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-16 15:51:02 +0200

    setting elements using OCR response works

--- components/clicking/image_processor/ocr.py
+++ components/clicking/image_processor/ocr.py
@@ -53,7 +53,7 @@ class OCR:
             request.id = str(clicking_image.id)
 
         batch_size = 20
-        responses = []
+        batch_responses = []
         total_batches = (len(batch_requests) + batch_size - 1) // batch_size
         batch_time = 0
 
@@ -64,43 +64,32 @@ class OCR:
                 client=self.client,
                 body=requests
             )
-            responses.extend(batch_response.responses)
+            batch_responses.extend(batch_response.responses)
 
             batch_time += batch_response.inference_time
             
         print(f"Batch time: {batch_time}")
-        print(f"Responses: {len(responses)}")
 
-        for response in responses:
-            print(f"Response: {response}")
-            for image in state.images:
-                if image.id != response.id:
-                    continue
-                for element in image.ui_elements:
-                    print(f"Element, {element.name}, label, {response.prediction.labels[0]}")    
-                    if element.name == response.prediction.labels[0]:
-                        print('Matched', element.name, response.prediction.labels[0])
-                    
+        for image in state.images:
+            for element in image.ui_elements:
+                for response in batch_responses:
 
+                    if image.id != response.id:
+                        continue
 
-            # try:
-            #     if not response.prediction or not response.prediction.quad_box:
-            #         print(f"No quad box found for {obj.name}")
-            #         obj.validity = ObjectValidity(status=ValidityStatus.INVALID, reason="No bounding box found")
-            #         continue
+                    for (bbox, label) in zip(response.prediction.bboxes, response.prediction.labels):
 
-            #     if len(response.prediction.quad_box) == 1:
-            #         obj.bbox = BoundingBox(bbox=response.prediction.quad_box[0], mode=BBoxMode.XYXY)
+                        # Skip if elements that are not named
+                        if element.name is None:
+                            continue
+  
+                        element_name = element.name.lower()
+                        label = label.lower()
 
-            #     else:
-            #         # If multiple bounding boxes are found, use the largest one
-            #         bboxes = [BoundingBox(bbox=bbox, mode=BBoxMode.XYXY) for bbox in response.prediction.quad_box]
-            #         obj.bbox = max(bboxes, key=lambda bbox: bbox.get_area())
-            #         print(f"Multiple bounding boxes found for {obj.name}: {len(response.prediction.quad_box)}. Using the largest one.")
+                        if element_name in label:
+                            element.bbox = bbox
+                            break
 
+        return state
 
-            # except Exception as e:
-            #     print(f"Error processing ocr for object {obj.name}: {str(e)}")
-        # return state
-
-        return responses
+        
