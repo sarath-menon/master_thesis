cf8958d7df90babcac7db140bfa13656b355db1c
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-09-26 16:32:10 +0200

    cleanup

--- init_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install dependencies
-apt update 
-apt install libgl1 mesa-utils python3.12-venvgit-lfs just
-
-# install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-echo 'export PATH="/root/.local/bin:$PATH"' >> ~/.bashrc
