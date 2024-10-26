ec06fca2bad0a08b4c2d3fba1eba01302ef0182a
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-10-26 10:11:16 +0200

    script for antropic clicking benchmark

---
+++ development/pipelines/monopoly_pipeline_anthropic.py
@@ -0,0 +1,113 @@
+#%%
+
+%load_ext autoreload
+%autoreload 2
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from typing import List, Dict, Tuple, Any
+from clicking.pipeline.core import Pipeline, PipelineState, PipelineStep, PipelineMode, PipelineModeSequence, PipelineModes
+from clicking.dataset_creator.core import CocoDataset
+from clicking.prompt_refinement.core import PromptRefiner, PromptMode
+from clicking.output_corrector.core import OutputCorrector, VerificationMode
+from clicking_client import Client
+from clicking.common.data_structures import *
+import asyncio
+import yaml
+from clicking.image_processor.visualization import show_localization_predictions, show_segmentation_predictions
+from io import BytesIO
+from clicking.image_processor.localization import Localization, LocalizerInput
+from clicking.image_processor.pointing import Pointing, PointingInput
+from clicking.image_processor.segmentation import Segmentation
+from clicking.image_processor.segmentation_text import SegmentationText
+from clicking.image_processor.anthropic_clicking import AnthropicClicking
+import nest_asyncio
+
+nest_asyncio.apply()
+#%%
+# Load the configuration file«
+CONFIG_PATH = "./development/pipelines/monopoly_config.yml"
+with open(CONFIG_PATH, 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+client = Client(base_url=config['api']['url'], timeout=120)
+anthropic_clicking = AnthropicClicking(client, config=config)
+#%%
+# Define the pipeline modes
+from clicking.output_corrector.core import VerificationMode  
+
+pipeline_modes = PipelineModes({
+    "pointing_mode": TaskType
+})
+
+# Create pipeline steps
+pipeline_steps = [
+    PipelineStep(
+        name="Filter categories",
+        function=lambda state: state.filter_by_object_category(ObjectCategory.GAME_ASSET),
+        mode_keys=[],
+    ),
+    PipelineStep(
+        name="Get Clickpoints",
+        function=anthropic_clicking.get_results,
+        mode_keys=["pointing_mode"]
+    ),
+]
+
+# Create pipeline and add steps
+pipeline = Pipeline(config=config)
+for step in pipeline_steps:
+    pipeline.add_step(step)
+
+# Create pipeline modes
+pipeline_mode_sequence = PipelineModeSequence.from_config(config, pipeline_modes)
+pipeline_mode_sequence.print_mode_sequences()
+
+#         obj.validity.status = ValidityStatus.UNKNOWN
+
+#%%
+coco_dataset = CocoDataset(config['dataset']['images_path'], config['dataset']['annotations_path'])
+clicking_images = coco_dataset.sample_dataset()
+
+all_results = asyncio.run(pipeline.run_for_all_modes(
+    initial_images=clicking_images,
+    # initial_state=loaded_state,
+    pipeline_modes=pipeline_mode_sequence,
+    # start_from_step="Filter categories",
+    # stop_after_step="Get Clickpoints"
+))
+
+# Print summary of results
+pipeline.print_mode_results_summary(all_results)
+
+result =  all_results.get_run_by_mode_name("direct_clickpoint") 
+
+#%%
+
+def plot_image_with_point(image, clickpoint, linewidth: int = 2):
+    plt.figure(figsize=(10, 8))
+    plt.imshow(image)
+    plt.grid(False)
+    plt.axis('off')
+    
+    # Draw crosshair lines spanning full image width/height
+    width = image.size[0]
+    height = image.size[1]
+    
+    plt.plot([0, width], [clickpoint.y, clickpoint.y], color='black', linewidth=linewidth, linestyle='dotted')
+    plt.plot([clickpoint.x, clickpoint.x], [0, height], color='black', linewidth=linewidth, linestyle='dotted')
+    plt.plot(clickpoint.x, clickpoint.y, marker='o', color='red', markersize=10)
+    print(f"Image size: {image.size}")
+    print(f"Clickpoint: {clickpoint}")
+    
+    plt.show()
+
+for clicking_image in result.images:
+    for obj in clicking_image.predicted_objects:
+        if obj.clickpoint is None:
+            print(f"Invalid clickpoint for object: {obj.name}")
+            continue
+        plot_image_with_point(clicking_image.image, obj.clickpoint)
+        print(f"Image {clicking_image.id}: {obj.name}")
+#%%
